b4eeac6622ccb4c821bea97d221cd37c849339f7
Prerna-09 <prerna09sijwali@gmail.com>
2024-05-12 22:55:36 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Temp from './Components/Weather/temp'
+
+
+function App() {
+  
+
+  return (
+    
+     <>
+       <Temp/>
+     </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
